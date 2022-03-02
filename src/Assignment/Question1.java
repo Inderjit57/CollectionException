4bd6981374dc617a484b63bfa734199115ccd4c4
Inderjit57 <singhinderjit57@yahoo.com>
2022-03-01 21:17:24 -0500

    completed

---
+++ src/Assignment/Question1.java
@@ -0,0 +1,36 @@
+package Assignment;
+
+/*
+Q1. You are given a phone book that consists of people's names and their phone number. 
+After that you will be given some person's name as input. For each input, print the phone number of that person. 
+For each case, print "Not found" if the person has no entry in the phone book. Otherwise, print the person's name and phone number.
+*/
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		HashMap<String, Integer> phoneList = new HashMap<String, Integer>();
+
+		phoneList.put("Sharukh", 223344);
+		phoneList.put("Amitabh", 112233);
+		phoneList.put("Akshay", 334455);
+		phoneList.put("Paresh", 445566);
+		phoneList.put("Ajay", 556677);
+
+		System.out.println("Enter person's name");
+		String name = sc.next();
+		for (int i = 0; i < phoneList.size(); i++) {
+			if (phoneList.containsKey(name)) {
+				System.out.println(phoneList.get(name));
+			} else {
+				System.out.println("Person is not found");
+				break;
+			}
+		}
+
+	}
+}
---
+++ src/Assignment/Question2.java
@@ -0,0 +1,33 @@
+package Assignment;
+
+import java.util.Scanner;
+
+/*
+ * Q2. You will be given two x & y as input, you have to compute x/y. 
+ * if x and y are not integers or if y is zero, exception will occur and you have to report it. 
+ */
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+		int x, y;
+		System.out.println("Enter x");
+		x = sc.nextInt();
+		System.out.println("Enter y");
+		y = sc.nextInt();
+		
+			if ((x%y)!= 0) {
+			System.out.println(x%y);	
+			}
+			
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Invalid entry");
+			e.printStackTrace();
+		}
+
+	}
+}
